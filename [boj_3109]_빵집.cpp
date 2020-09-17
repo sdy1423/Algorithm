260e38f9d3ed94159cacc14d3f0363290058b94b
sdy1423 <53420624+sdy1423@users.noreply.github.com>
2020-09-17 18:08:55 +0900

    Update [boj_3109]_빵집.cpp
    굳이 visited배열이 필요하지 않아서 코드를 수정했다. 방문을 하면 mp배열에 있는 값을 '.'에서 'x'으로 바꿔주도록 수정했다.

--- [boj_3109]_빵집.cpp
+++ [boj_3109]_빵집.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int r, c, ans;
 char mp[10001][501];
-bool visited[10001][501];
-int dx[] = { -1,0,1 };
-int dy[] = { 1,1,1 };
+int dx[] = { -1,0,1 }, dy[] = { 1,1,1 };
 bool Check(int nx, int ny) {
 	if (nx >= 0 && ny >= 0 && nx < r && ny < c)return true;
 	else return false;
@@ -18,16 +16,13 @@ bool Go(int x, int y) {
 		int ny = y + dy[dir];
 		if (!Check(nx, ny))continue;
 		if (mp[nx][ny] == 'x')continue;
-		if (!visited[nx][ny]) {
-			visited[nx][ny] = true;
-			bool ok = Go(nx, ny);
-			if (ok)return true;
-		}
+		mp[nx][ny] = 'x';
+		if (Go(nx, ny))return true;
 	}
 	return false;
 }
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> r >> c;
 	for (int i = 0; i < r; i++)
 		for (int j = 0; j < c; j++)
